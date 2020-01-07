b3fb219e3faba20a69a2dd0b310a5982c8d6ab4c
Sridar Kandaswamy <skandasw@cisco.com>
2020-01-07 11:16:50 -0800

    Enable SVI networks with hosts running opflex agent
    - Allow port binding on SVI networks with hosts running opflex agent
    - Supported with vlan type networks (not opflex networks)
    - Provides a flag indicating SVI network, the vlan id and the epg is
      set to a unique id (SVI network id) in response to get_gbp_details.
      This enables the opflex agent on the host to set up the data path
      appropriately.
    
    Change-Id: Ia0a3e4354141b04693fda8c1079acbbc76f371d9
    (cherry picked from commit 5aee410131ea8d71d73cad72cb8421f580aa4a3d)
    (cherry picked from commit 0f3e3fb25886ea300034015f24412bb3f83fb8e0)

--- gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/rpc.py
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/rpc.py
@@ -72,6 +72,7 @@ EndpointPortInfo = namedtuple(
      'nested_domain_infra_vlan',
      'nested_domain_service_vlan',
      'nested_domain_node_network_vlan',
+     'svi',
      'epg_name',
      'epg_app_profile_name',
      'epg_tenant_name',
@@ -96,7 +97,8 @@ EndpointBindingInfo = namedtuple(
     ['host',
      'level',
      'network_type',
-     'physical_network'])
+     'physical_network',
+     'segmentation_id'])
 
 EndpointSecurityGroupInfo = namedtuple(
     'EndpointSecurityGroupInfo',
@@ -497,6 +499,7 @@ class ApicRpcHandlerMixin(object):
             extension_db.NetworkExtensionDb.nested_domain_service_vlan,
             extension_db.NetworkExtensionDb.
             nested_domain_node_network_vlan,
+            extension_db.NetworkExtensionDb.svi,
             db.NetworkMapping.epg_name,
             db.NetworkMapping.epg_app_profile_name,
             db.NetworkMapping.epg_tenant_name,
@@ -588,6 +591,7 @@ class ApicRpcHandlerMixin(object):
             ml2_models.PortBindingLevel.level,
             segment_models.NetworkSegment.network_type,
             segment_models.NetworkSegment.physical_network,
+            segment_models.NetworkSegment.segmentation_id,
         ))
         query += lambda q: q.join(
             segment_models.NetworkSegment,
@@ -884,6 +888,7 @@ class ApicRpcHandlerMixin(object):
         details['network_id'] = port_info.network_id
         details['network_type'] = binding_info[-1].network_type
         details['physical_network'] = binding_info[-1].physical_network
+        details['segmentation_id'] = binding_info[-1].segmentation_id
         details['port_id'] = port_info.port_id
 
         return details
@@ -918,7 +923,11 @@ class ApicRpcHandlerMixin(object):
         details['dns_domain'] = port_info.net_dns_domain or ''
         details['enable_dhcp_optimization'] = self.enable_dhcp_opt
         details['enable_metadata_optimization'] = self.enable_metadata_opt
-        details['endpoint_group_name'] = port_info.epg_name
+        if port_info.svi:
+            details['endpoint_group_name'] = port_info.network_id
+            details['svi'] = True
+        else:
+            details['endpoint_group_name'] = port_info.epg_name
         details['floating_ip'] = self._build_fips(info)
         details['host'] = port_info.host
         details['host_snat_ips'] = self._build_host_snat_ips(
