77a682d69a147315c952aece795ce9fbf0133b90
ansao-aci <ansao@cisco.com>
2021-09-09 13:32:36 +0530

    System security grp:Add system sg in port sg list
    Change-Id: Ib0e07aeb1f967b7d3f5035f1b5e0404eece81b73
    (cherry picked from commit 7eaa0829f5a0f18f8365cf5b362718e2942d4131)

--- gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
+++ gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
@@ -2930,6 +2930,10 @@ class TestGbpDetailsForML2(AIMBaseTestCase,
         sg_list.append({'policy-space': 'common',
                         'name': self.mech_driver.apic_system_id +
                         '_DefaultSecurityGroup'})
+        sg_list.append({'policy-space': 'common',
+                        'name': self.mech_driver.apic_system_id +
+                        '_SystemSecurityGroup'})
+
         self.assertEqual(sg_list, mapping['security_group'])
         # Set the right MTU through extra_dhcp_opts
         data = {'port': {'extra_dhcp_opts': [{'opt_name': 'interface-mtu',
@@ -3468,6 +3472,10 @@ class TestPolicyTarget(AIMBaseTestCase,
         sg_list.append({'policy-space': 'common',
                         'name': self.mech_driver.apic_system_id +
                         '_DefaultSecurityGroup'})
+        sg_list.append({'policy-space': 'common',
+                        'name': self.mech_driver.apic_system_id +
+                        '_SystemSecurityGroup'})
+
         self.assertEqual(sg_list, mapping['security_group'])
         # Set the right MTU through extra_dhcp_opts
         data = {'port': {'extra_dhcp_opts': [{'opt_name': 'interface-mtu',
