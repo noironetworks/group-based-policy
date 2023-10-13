f305996857872d81f121dd24a1a755b989571b52
Thomas Bachman <tbachman@yahoo.com>
2023-10-13 02:42:29 +0000

    Revert "Fix Log.warn"
    This reverts commit 953997a9a8060cafef991067ab2cd23182ec008f.
    The patch reverts the change of warn to warning in alembic_mgrations.
    The alembic utils library only has warn, and not warning, so this
    patch isn't needed.
    
    Change-Id: Ibd16c88ea33ae668316506c58348ce2b5c1a53d6

--- gbpservice/neutron/db/migration/alembic_migrations/versions/68fcb81878c5_alter_ha_ip_address_table.py
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/68fcb81878c5_alter_ha_ip_address_table.py
@@ -45,12 +45,11 @@ def upgrade():
                 data_migrations.do_ha_ip_duplicate_entries_removal(session)
                 data_migrations.do_ha_ip_network_id_insertion(session)
         except ImportError:
-            util.warning("AIM schema present, but failed to import"
-                        "AIM libraries - HA IP duplicate entries"
-                        "removal not completed.")
+            util.warn("AIM schema present, but failed to import AIM libraries"
+                      " - HA IP duplicate entries removal not completed.")
         except Exception as e:
-            util.warning("Caught exception while migrating data in %s: %s"
-                        % ('apic_ml2_ha_ipaddress_to_port_owner', e))
+            util.warn("Caught exception while migrating data in %s: %s"
+                      % ('apic_ml2_ha_ipaddress_to_port_owner', e))
 
         inspector = reflection.Inspector.from_engine(op.get_bind())
         pk_constraint = inspector.get_pk_constraint(
